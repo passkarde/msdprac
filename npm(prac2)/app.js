904bfc1532cd57449d0e72768c42d83e8aa6ae2e
MSG <krishnayt012@gmail.com>
2026-04-05 21:13:15 +0530

    Update app.js

--- npm(prac2)/app.js
+++ npm(prac2)/app.js
@@ -1,2 +1,12 @@
 console.log("My First npm Project");
 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("My first npm project!");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
