b917130abfcb9010a2753bf29fa035791b5c0a27
MSG <krishnayt012@gmail.com>
2026-04-05 21:17:49 +0530

    Update app.js

--- module-demo(3)(multiply,add,etc)/app.js
+++ module-demo(3)(multiply,add,etc)/app.js
@@ -3,3 +3,25 @@ const math =require('./math');
 console.log("Addition:", math.add(10,20));
 console.log("Multiplication:", math.multiply(5,6));
 console.log("Substraction:", math.sub(20,10));
+
+// const express = require('express');
+// // const math = require('./math');
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     const sum = math.add(10, 20);
+//     const product = math.multiply(5, 6);
+//     const sub = math.sub(20,10);
+
+//     res.send(`
+//         <h2>Results</h2>
+//         <p>Addition: ${sum}</p>
+//         <p>Multiplication: ${product}</p>
+//          <p>Substraction: ${sub}</p>
+//     `);
+// });
+
+// app.listen(3000, () => {
+//     console.log("Server running on http://localhost:3000");
+// });
