15e0c319bd9d36e17bf4f43035cd2816c7abf49b
MSG <krishnayt012@gmail.com>
2026-04-06 08:42:09 +0530

    Update app.ts

--- prac14(Angular Routing)/src/app/app.ts
+++ prac14(Angular Routing)/src/app/app.ts
@@ -1,10 +1,21 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet], // 👈 IMPORTANT
+//   templateUrl: './app.html'
+// })
+// export class App {}
+
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet], // 👈 IMPORTANT
-  templateUrl: './app.html'
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './app.html',
 })
 export class App {}
